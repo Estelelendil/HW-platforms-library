a0c676c8fa486ba841b0e1aa5e9ee08acdf83a32
Чернова Александра <estelelendil@mail.ru>
2022-07-12 18:44:56 +0300

    dependens installed

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    mode: 'production',
+    mode: {
+        rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: [
+                    ['@babel/preset-env', { targets: "defaults" }]
+                  ]
+                }
+              }
+            }
+          ]
+
+    }
+}
